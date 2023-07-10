63006d1de3c33441e8683fa8beb5b478cdd5b650
psmuler <zhmuler@gmail.com>
2023-07-10 09:56:34 +0900

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+// App.tsx
+import React, { useEffect, useState } from "react";
+import SortableList from "./components/SortableList";
+import ComboBox from "./components/AutoComplete"
+
+interface Stack {
+    order: number;
+    text: string;
+}
+
+function App() {
+    const [stacks, setStacks] = useState<Stack[]>([]);
+
+    useEffect(() => {
+        fetch('stacks.json')
+            .then(response => response.json())
+            .then(data => {
+                data.sort((a: Stack, b: Stack) => a.order - b.order);
+                setStacks(data);
+            });
+    }, []);
+
+    const handleTextChange = (order: number, text: string) => {
+        const newStacks = stacks.map(stack =>
+            stack.order === order ? { ...stack, text } : stack
+        );
+        setStacks(newStacks);
+        localStorage.setItem('stacks', JSON.stringify(newStacks));
+    };
+
+    return (
+        <div>
+            <SortableList stacks={stacks} onTextChange={handleTextChange} />
+            <ComboBox />
+        </div>
+    );
+}
+
+export default App;

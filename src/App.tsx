512afed0c539e91e269882739fd6eb36a8804ed0
psmuler <zhmuler@gmail.com>
2023-07-12 00:26:28 +0900

    cssusage

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
-// App.tsx
-import React, { useEffect, useState } from "react";
-import SortableList from "./components/SortableList";
-import ComboBox from "./components/AutoComplete"
+import './App.css';
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
 
-interface Stack {
-    order: number;
-    text: string;
-}
-
-function App() {
-    const [stacks, setStacks] = useState<Stack[]>([]);
-
-    useEffect(() => {
-        fetch('stacks.json')
-            .then(response => response.json())
-            .then(data => {
-                data.sort((a: Stack, b: Stack) => a.order - b.order);
-                setStacks(data);
-            });
-    }, []);
-
-    const handleTextChange = (order: number, text: string) => {
-        const newStacks = stacks.map(stack =>
-            stack.order === order ? { ...stack, text } : stack
-        );
-        setStacks(newStacks);
-        localStorage.setItem('stacks', JSON.stringify(newStacks));
-    };
-
+export default function  Profile() {
     return (
-        <div>
-            <SortableList stacks={stacks} onTextChange={handleTextChange} />
-            <ComboBox />
-        </div>
+      <>
+        <h1 className='Top'>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
     );
-}
-
-export default App;
+  }
+  
+  
